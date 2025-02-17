0f44b13da02cb92bb9a74c9af97c5253ecb71f9e
c0mplex <snakebigbosslel@gmail.com>
2025-02-18 01:43:20 +0800

    Add project files.

---
+++ FitLife/Components/Pages/Login/Login.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitLife.Components.Pages.Login;
+
+public partial class Login
+{
+    private UserCredential Model = new();
+}
+
+public class UserCredential
+{
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
