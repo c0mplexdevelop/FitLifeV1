e7c63f5254c96c0017c5687c4ea9484ba25c0436
c0mplex <snakebigbosslel@gmail.com>
2025-03-18 18:57:25 +0800

    Login Page front-end done. Functionality WIP

--- FitLife/Components/Pages/Login/Login.razor.cs
+++ FitLife/Components/Pages/Login/Login.razor.cs
@@ -2,14 +2,10 @@
 
 namespace FitLife.Components.Pages.Login;
 
+using FitLife.Models.User;
+
 public partial class Login
 {
     private UserCredential Model = new();
 }
 
-public class UserCredential
-{
-    public string LoginIdentifier { get; set; } = null!;
-
-    public string Password { get; set; } = null!;
-}
---
+++ FitLife/Models/User/UserCredential.cs
@@ -0,0 +1,8 @@
+﻿namespace FitLife.Models.User;
+
+public class UserCredential
+{
+    public string LoginIdentifier { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
