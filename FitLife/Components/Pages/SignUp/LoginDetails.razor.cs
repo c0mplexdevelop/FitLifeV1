2c67e8ea363fbdf6f9825e162ba1f4b9935255fa
c0mplex <snakebigbosslel@gmail.com>
2025-03-18 18:57:50 +0800

    Sign Up page first page WIP, foundational structure done

---
+++ FitLife/Components/Pages/SignUp/LoginDetails.razor.cs
@@ -0,0 +1,10 @@
+﻿using FitLife.Models.User;
+using Microsoft.AspNetCore.Components;
+
+namespace FitLife.Components.Pages.SignUp;
+
+public partial class LoginDetails
+{
+    [SupplyParameterFromForm]
+    private UserCredential? Model { get; set; } = new UserCredential();
+}
---
+++ FitLife/Models/User/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitLife.Models.User;
+
+public class User
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [MaxLength(64)]
+    public string FirstName { get; set; } = null!;
+
+    [MaxLength(64)]
+    public string? MiddleName { get; set; }
+
+    [Required]
+    [MaxLength(64)]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    [StringLength(16, MinimumLength = 6)]
+    public string Username { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+}
