662ff7310cfa5feccceca9a1622c01bba264e660
c0mplex <snakebigbosslel@gmail.com>
2025-03-24 22:28:02 +0800

    Added unit tests for automated testing

---
+++ FitlifeTest/DatabaseTest.cs
@@ -0,0 +1,49 @@
+﻿using FitLife.Data.Repository.Interface;
+using FitLife.Models.User;
+using FitLife.Models.User.Enum;
+using Moq;
+
+namespace FitlifeTest;
+
+public class DatabaseTest
+{
+    [Fact]
+    public async Task GetByIdAsync_Returns_ValidUser()
+    {
+        //Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+        User expectedUser = new()
+        {
+            Id = -1,
+            Email = "test@test.com",
+            FirstName = "John",
+            MiddleName = null,
+            LastName = "Doe",
+            Sex = Sex.Male,
+            DateOfBirth = DateOnly.MinValue,
+            Username = "John Doe",
+            Password = "Test!23"
+        };
+
+        mockUserRepository.Setup(repo => repo.GetByIdAsync(-1)).ReturnsAsync(expectedUser);
+
+        //Act
+        var result = await mockUserRepository.Object.GetByIdAsync(-1);
+
+        //Assert
+        Assert.Equal(expectedUser, result);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_Returns_Null()
+    {
+        //Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+        User? expectedUser = null;
+        mockUserRepository.Setup(repo => repo.GetByIdAsync(-1)).ReturnsAsync(expectedUser);
+        //Act
+        var result = await mockUserRepository.Object.GetByIdAsync(-1);
+        //Assert
+        Assert.Equal(expectedUser, result);
+    }
+}
---
+++ FitlifeTest/UserAuthTest.cs
@@ -0,0 +1,138 @@
+﻿using Xunit;
+using Moq;
+using FitLife.Data.Repository.Interface;
+using FitLife.Models.User;
+using FitLife.Models.User.Enum;
+using FitLife.Auth;
+
+
+namespace FitlifeTest;
+
+public class UserAuthTest
+{
+    User testUser = new()
+    {
+        Id = -1,
+        Email = "test@test.com",
+        FirstName = "John",
+        MiddleName = null,
+        LastName = "Doe",
+        Sex = Sex.Male,
+        DateOfBirth = DateOnly.MinValue,
+        Username = "John Doe",
+        Password = "Test!23"
+    };
+    [Fact]
+    public async Task LoginAsyncUsingUsername_ReturnsTrue_WhenCredentialsAreCorrect()
+    {
+        // Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User> { testUser });
+
+        var authService = new AuthService(mockUserRepository.Object);
+
+        var testCredentials = new UserLoginCredential
+        {
+            LoginIdentifier = "John Doe",
+            Password = "Test!23"
+        };
+
+        // Act
+        var result = await authService.LoginAsync(testCredentials);
+
+        // Assert
+        Assert.Equal(testUser, result);
+    }
+
+    [Fact]
+    public async Task LoginAsyncUsingEmail_ReturnsTrue_WhenCredentialsAreCorrect()
+    {
+        // Arrange
+        // Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User> { testUser });
+
+        var authService = new AuthService(mockUserRepository.Object);
+
+        var testCredentials = new UserLoginCredential
+        {
+            LoginIdentifier = "test@test.com",
+            Password = "Test!23"
+        };
+
+        // Act
+        var result = await authService.LoginAsync(testCredentials);
+
+        // Assert
+        Assert.Equal(testUser, result);
+    }
+
+    [Fact]
+    public async Task LoginAsyncUsingUsername_ReturnsNull_WhenPasswordisIncorrect()
+    {
+        // Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User> { testUser });
+
+        var authService = new AuthService(mockUserRepository.Object);
+
+        var testCredentials = new UserLoginCredential
+        {
+            LoginIdentifier = "John Doe",
+            Password = "Test!24"
+        };
+
+        // Act
+        var result = await authService.LoginAsync(testCredentials);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task LoginAsyncUsingUsername_ReturnsNull_WhenUsernameIsIncorrect()
+    {
+        // Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+
+        mockUserRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User> { testUser });
+
+        var authService = new AuthService(mockUserRepository.Object);
+
+        var testCredentials = new UserLoginCredential
+        {
+            LoginIdentifier = "John Doe1",
+            Password = "Test!23"
+        };
+
+        // Act
+        var result = await authService.LoginAsync(testCredentials);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task LoginAsyncUsingEmail_ReturnsNull_WhenEmailIsIncorrect()
+    {
+        // Arrange
+        var mockUserRepository = new Mock<IUserRepository>();
+        mockUserRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<User> { testUser });
+        var authService = new AuthService(mockUserRepository.Object);
+        var testCredentials = new UserLoginCredential
+        {
+            LoginIdentifier = "tesst@gmail.com",
+            Password = "Test!23"
+        };
+
+        // Act
+        var result = await authService.LoginAsync(testCredentials);
+
+        // Assert
+        Assert.Null(result);
+
+    }
+}
