5afc28f8ed702fdec295630ab2d914567a2ccab3
c0mplex <snakebigbosslel@gmail.com>
2025-03-28 14:52:41 +0800

    Changed minimum password length to 6

--- FitLife/Program.cs
+++ FitLife/Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
     options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 8;
+    options.Password.RequiredLength = 6;
     options.User.RequireUniqueEmail = true;
 })
     .AddEntityFrameworkStores<DatabaseContext>()
