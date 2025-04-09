b37cc5688b34554ce90d6cda263856f1691fc168
c0mplex <snakebigbosslel@gmail.com>
2025-04-10 01:06:52 +0800

    Created Exercise model class

---
+++ FitLife/Models/Exercises/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using FitLife.Models.Exercises.Enums;
+
+namespace FitLife.Models.Exercises;
+
+public class Exercise
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name = string.Empty;
+    //public string Description = string.Empty;
+    public string Type = string.Empty;
+    public ExerciseDifficulty Difficulty;
+    public string TargetMuscleGroup = string.Empty;
+    public string EquipmentNeeded = string.Empty;
+    public int Reps { get; set; }
+    public int DurationInSeconds { get; set; }
+    public int Sets { get; set; }
+
+}
