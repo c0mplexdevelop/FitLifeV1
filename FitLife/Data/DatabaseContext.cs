4b0081d3e79288e213b81b2c20e3e3a258947ce3
c0mplex <snakebigbosslel@gmail.com>
2025-04-10 19:19:26 +0800

    Reworked to dynamically load the  exercises on the front end

--- FitLife/Data/DatabaseContext.cs
+++ FitLife/Data/DatabaseContext.cs
@@ -35,27 +35,25 @@ public class DatabaseContext: IdentityDbContext<User, IdentityRole<int>, int>
         User? user = Set<User>().Find(-1);
         if (user == null)
         {
-            return;
-        }
-        user = new()
-        {
-            Id = -1,
-            Email = "test@test.com",
-            NormalizedEmail = "test@test.com".ToUpper(),
-            FirstName = "John",
-            MiddleName = null,
-            LastName = "Doe",
-            Sex = FitLife.Models.User.Enum.Sex.Male,
-            DateOfBirth = DateOnly.MinValue,
-            UserName = "JohnDoe",
-            NormalizedUserName = "JohnDoe".ToUpper(),
-            SecurityStamp = Guid.NewGuid().ToString()
-        };
-        user.PasswordHash = new PasswordHasher<User>().HashPassword(user, "Test!23");
-        Set<User>().Add(user);
+            user = new()
+            {
+                Id = -1,
+                Email = "test@test.com",
+                NormalizedEmail = "test@test.com".ToUpper(),
+                FirstName = "John",
+                MiddleName = null,
+                LastName = "Doe",
+                Sex = FitLife.Models.User.Enum.Sex.Male,
+                DateOfBirth = DateOnly.MinValue,
+                UserName = "JohnDoe",
+                NormalizedUserName = "JohnDoe".ToUpper(),
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            user.PasswordHash = new PasswordHasher<User>().HashPassword(user, "Test!23");
+            Set<User>().Add(user);
         }
         
-
+       
         if(!Set<Exercise>().Any())
         {
             var csvPath = Path.Combine(Directory.GetCurrentDirectory(), "ExerciseTable.csv");
