d9f7af85645fd58f47b60b0a964f77fa7b3c8160
c0mplex <snakebigbosslel@gmail.com>
2025-04-13 23:35:06 +0800

    Display the username when user logged in

--- FitLife/Components/Pages/UserDashboard/UserDashboard.razor.cs
+++ FitLife/Components/Pages/UserDashboard/UserDashboard.razor.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FitLife.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
-namespace FitLife.Components.Pages.UserDashboard
+namespace FitLife.Components.Pages.UserDashboard;
+
+public partial class UserDashboard
 {
-    [Authorize]
-    public partial class UserDashboard
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
+
+    private string userName = string.Empty;
+
+    [Inject]
+    private ILogger<UserDashboard> Logger { get; set; } = null!;
+
+    [Inject]
+    private DatabaseContext DbContext { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
     {
+        if (AuthenticationStateTask is null)
+        {
+            throw new InvalidOperationException("AuthenticationStateTask is null, ensure the routes is wrapped in CascadingAuthenticationState");
+        }
+
+        var authState = await AuthenticationStateTask;
+        var user = authState.User;
+         userName = user.Identity!.Name;
+        await base.OnInitializedAsync();
     }
 }
