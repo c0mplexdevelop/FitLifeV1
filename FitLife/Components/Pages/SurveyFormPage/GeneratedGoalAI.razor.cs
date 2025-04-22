8b448fa043761a0f66be79219bda2fa6abea648e
AJ Sen <sen.alexanderjonard.deslate@gmail.com>
2025-04-22 20:30:33 +0800

    UI of AI Generated Goal  after submitting the survey

---
+++ FitLife/Components/Pages/SurveyFormPage/GeneratedGoalAI.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace FitLife.Components.Pages.SurveyFormPage
+{
+    public partial class GeneratedGoalAI
+    {
+        private bool isLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await Task.Delay(TimeSpan.FromSeconds(4));
+
+            isLoading = false;
+        }
+    }
+}
