52b5686f229a0a01790cfd1df61174841686eb5a
c0mplex <snakebigbosslel@gmail.com>
2025-04-25 02:19:11 +0800

    WIP setup of recommendation

--- FitLife/Components/Pages/SurveyFormPage/GeneratedGoalAI.razor.cs
+++ FitLife/Components/Pages/SurveyFormPage/GeneratedGoalAI.razor.cs
@@ -1,15 +1,48 @@
-﻿namespace FitLife.Components.Pages.SurveyFormPage
+﻿using FitLife.Data;
+using FitLife.Models.Exercises;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.ML;
+
+namespace FitLife.Components.Pages.SurveyFormPage;
+
+public partial class GeneratedGoalAI
 {
-    public partial class GeneratedGoalAI
+    private const string MODEL_PATH = "AIModel.zip";
+
+    private bool isLoading = true;
+
+    private ITransformer? model;
+
+    [Inject]
+    private DatabaseContext dbContext { get; set; }
+
+    private List<Exercise> exercises = new List<Exercise>();
+    protected override async Task OnInitializedAsync()
     {
-        private bool isLoading = true;
+        await base.OnInitializedAsync();
+        await Task.Delay(TimeSpan.FromSeconds(4));
 
-        protected override async Task OnInitializedAsync()
+        MLContext mlContext = new MLContext();
+        model = LoadModel(mlContext);
+        exercises = await dbContext.Exercises.AsNoTracking().ToListAsync();
+        if (model == null)
         {
-            await base.OnInitializedAsync();
-            await Task.Delay(TimeSpan.FromSeconds(4));
-
-            isLoading = false;
+            Console.WriteLine("Model is null");
+            return;
         }
+
+        //TODO: Simulate data here
+        var predictionEngine = mlContext.Model.CreatePredictionEngine<SurveyForm, ExercisePrediction>(model);
+        isLoading = false;
+    }
+
+    public static ITransformer LoadModel(MLContext mlContext)
+    {
+        string modelPath = Path.Combine(Environment.CurrentDirectory, MODEL_PATH);
+        ITransformer loadedModel = mlContext.Model.Load(modelPath, out DataViewSchema modelSchema);
+        Console.WriteLine($"Model loaded from {modelPath}");
+
+        return loadedModel;
     }
 }
