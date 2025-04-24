6c723719d79ea7675b301162b9f7c55571c3c7ec
c0mplex <snakebigbosslel@gmail.com>
2025-04-25 02:18:55 +0800

    WIP set up of survey

--- FitLife/Components/Pages/SurveyFormPage/SurveyPage2.razor.cs
+++ FitLife/Components/Pages/SurveyFormPage/SurveyPage2.razor.cs
@@ -4,9 +4,8 @@ using Microsoft.AspNetCore.Components;
 
 namespace FitLife.Components.Pages.SurveyFormPage;
 
-public partial class SurveyPage1
+public partial class SurveyPage2
 {
-
     [Inject]
     private NavigationManager NavigationManager { get; set; } = default!;
 
@@ -24,6 +23,6 @@ public partial class SurveyPage1
     {
         // Save the survey model to the database or perform any other action
         // For example, you can navigate to another page after submission
-        NavigationManager.NavigateTo("/survey-page-2");
+        NavigationManager.NavigateTo("/survey-page-3");
     }
 }
