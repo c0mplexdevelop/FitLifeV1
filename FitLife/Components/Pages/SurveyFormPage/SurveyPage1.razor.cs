8a388dcb21018bbb9bf3fc07e61735d755ca702e
c0mplex <snakebigbosslel@gmail.com>
2025-04-27 00:49:40 +0800

    Implemented retrieval of User Gender for Model Input later

--- FitLife/Components/Pages/SurveyFormPage/SurveyPage1.razor.cs
+++ FitLife/Components/Pages/SurveyFormPage/SurveyPage1.razor.cs
@@ -1,6 +1,9 @@
-﻿using FitLife.Models.Survey;
+﻿using FitLife.Auth;
+using FitLife.Data;
+using FitLife.Models.Survey;
 using FitLife.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitLife.Components.Pages.SurveyFormPage;
 
@@ -13,17 +16,32 @@ public partial class SurveyPage1
     [Inject]
     private SurveyService _surveyService { get; set; } = default!;
 
+    [Inject]
+    private DatabaseContext dbContext { get; set; } = default!;
+
+    [Inject]
+    private AuthService _authService { get; set;} = default!;
     private SurveyModel? _surveyModel;
+
+    [Inject]
+    private ILogger<SurveyPage1> _logger { get; set; } = default!;
 
     protected override void OnInitialized()
     {
+        _surveyService.ResetSurveyModel();
         _surveyModel = _surveyService.SurveyModel;
     }
 
-    private void OnValidSubmit()
+    private async Task OnValidSubmit()
     {
         // Save the survey model to the database or perform any other action
         // For example, you can navigate to another page after submission
+        var username = await _authService.ReturnUserName();
+        var user = await dbContext.Users.AsNoTracking()
+            .FirstOrDefaultAsync(u => u.UserName == username);
+        _surveyModel.Gender = user.Sex.ToString();
+        _logger.LogWarning($"Var surveymodel: {_surveyModel.Age}");
+        _logger.LogWarning($"Service surveyModel: {_surveyService.SurveyModel.Age}");
         NavigationManager.NavigateTo("/survey-page2");
     }
 }
