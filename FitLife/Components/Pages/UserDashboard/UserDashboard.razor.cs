56a6dd00bc57f1f82b27f0a32bd8aa0c0476091d
c0mplex <snakebigbosslel@gmail.com>
2025-04-27 20:07:13 +0800

    Implemented features of archiving exercises

--- FitLife/Components/Pages/UserDashboard/UserDashboard.razor.cs
+++ FitLife/Components/Pages/UserDashboard/UserDashboard.razor.cs
@@ -1,8 +1,12 @@
 ﻿using FitLife.Auth;
 using FitLife.Data;
+using FitLife.Models.Exercises;
+using FitLife.Models.Intermediary;
+using FitLife.Models.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitLife.Components.Pages.UserDashboard;
 
@@ -22,6 +26,9 @@ public partial class UserDashboard
     [Inject]
     private AuthService AuthService { get; set; } = null!;
 
+    private List<UserExerciseSubscription> UserExercisesSubscriptions { get; set; } = new();
+
+    private int completedWorkoutsCount = 0;
     protected override async Task OnInitializedAsync()
     {
         if (AuthenticationStateTask is null)
@@ -29,6 +36,76 @@ public partial class UserDashboard
             throw new InvalidOperationException("AuthenticationStateTask is null, ensure the routes is wrapped in CascadingAuthenticationState");
         }
 
-        userName = await AuthService.ReturnUserName();
+        var user = await AuthService.GetCurrentUser();
+        userName = user.UserName ?? "N/A";
+
+        UserExercisesSubscriptions = await DbContext.UserExerciseSubscriptions
+                .Include(ues => ues.Exercise)
+                .Where(ues => ues.UserId == user.Id)
+                .ToListAsync();
+
+        completedWorkoutsCount = await DbContext.UserExerciseHistory
+            .CountAsync(ueh => ueh.UserId == user.Id && ueh.IsCompleted);
+    }
+
+    public async Task CompleteExercise(UserExerciseSubscription subscription)
+    {
+        var user = await AuthService.GetCurrentUser();
+        var exercise = subscription.Exercise;
+        if (exercise == null)
+        {
+            Logger.LogError($"Exercise with ID {subscription.ExerciseId} not found.");
+            return;
+        }
+
+        var refetchedSubscription = await DbContext.UserExerciseSubscriptions
+            .Include(ues => ues.Exercise)
+            .FirstOrDefaultAsync(ues => ues.UserId == subscription.UserId && ues.ExerciseId == subscription.ExerciseId);
+        if(refetchedSubscription == null)
+        {
+            UserExercisesSubscriptions.Remove(subscription);
+            StateHasChanged();
+            return;
+        }
+        var userExerciseHistory = CreateExerciseHistory(subscription, user, true);
+        DbContext.UserExerciseSubscriptions.Remove(refetchedSubscription);
+        DbContext.UserExerciseHistory.Add(userExerciseHistory);
+        await DbContext.SaveChangesAsync();
+        UserExercisesSubscriptions.Remove(subscription);
+    }
+
+    public async Task DeleteExercise(UserExerciseSubscription subscription)
+    {
+        var user = await AuthService.GetCurrentUser();
+        var exercise = subscription.Exercise;
+        if (exercise == null)
+        {
+            Logger.LogError($"Exercise with ID {subscription.ExerciseId} not found.");
+            return;
+        }
+        var refetchedSubscription = await DbContext.UserExerciseSubscriptions
+                    .Include(ues => ues.Exercise)
+                    .FirstOrDefaultAsync(ues => ues.UserId == subscription.UserId && ues.ExerciseId == subscription.ExerciseId);
+        if (refetchedSubscription == null)
+        {
+            UserExercisesSubscriptions.Remove(subscription);
+            StateHasChanged();
+            return;
+        }
+        var userExerciseHistory = CreateExerciseHistory(subscription, user, true);
+        DbContext.UserExerciseSubscriptions.Remove(refetchedSubscription);
+        DbContext.UserExerciseHistory.Add(userExerciseHistory);
+        await DbContext.SaveChangesAsync();
+        UserExercisesSubscriptions.Remove(subscription);
+    }
+
+    private UserExerciseHistory CreateExerciseHistory(UserExerciseSubscription subscription, User user, bool isComplete)
+    {
+        return new UserExerciseHistory
+        {
+            UserId = user.Id,
+            ExerciseId = subscription.Exercise.Id,
+            IsCompleted = isComplete
+        };
     }
 }
